e4c9d89f336a06c5fca6e3b58037faf0a0382d84
kavirasan <kavirasan1998@gmail.com>
2024-11-23 12:35:38 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        {/* <Route path="/" element={<Home/>}/> */}
        
         </Routes>
       </BrowserRouter>
